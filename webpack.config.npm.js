4642779284311586a48471910f8a42d7bd5077a7
Drew Thorson <thorsdre@gmail.com>
2016-07-01 14:55:23 -0500

    trying to compile separately each js file

--- webpack.config.npm.js
+++ webpack.config.npm.js
@@ -2,40 +2,62 @@ import webpack from 'webpack';
 import path from 'path';
 import TransferWebpackPlugin from 'transfer-webpack-plugin';
 
+// __dirname is referencing the name of the directory that the currently executing script resides in
+
 const config = {
-  entry: __dirname + '/src/app/d3components',
+  entry: [
+    __dirname + '/src/app/d3components/index.js'
+  ],
   output: {
     filename: '[name].js',
-    path: __dirname + '/lib'
+    path: __dirname + '/lib',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
   },
   devtool: 'source-map',
   noInfo: false,
   plugins: [
-    new TransferWebpackPlugin([
-      {from: 'app/d3components'}
-    ], path.resolve(__dirname,"src")),
-    new TransferWebpackPlugin([
-      {from: ''}
-    ], path.resolve(__dirname,"build"))
+    // new webpack.optimize.OccurenceOrderPlugin(),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compressor: {
+    //     screw_ie8: true,
+    //     keep_fnames: false,
+    //     warnings: false
+    //   },
+    //   mangle: {
+    //     screw_ie8: true,
+    //     keep_fnames: false
+    //   }
+    // }),
+    // new TransferWebpackPlugin([
+    //   {from: 'app/d3components'}
+    // ], path.resolve(__dirname,"src")),
+    // new TransferWebpackPlugin([
+    //   {from: ''}
+    // ], path.resolve(__dirname,"build"))
   ],
   module: {
     loaders: [
       {
-        // "test" is commonly used to match the file extension
-        test: /(\.js|\.jsx)$/,
+        // the "loader"
+        loader: "babel-loader", // or "babel" because webpack adds the '-loader' automatically
 
         // "include" is commonly used to match the directories
         include: [
-          path.resolve(__dirname, "src/app")
+          path.resolve(__dirname, "src/app/d3components")
         ],
 
         // "exclude" should be used to exclude exceptions
         // try to prefer "include" when possible
+        // exclude: [
+        //   path.resolve(__dirname, "src/app/d3components")
+        // ],
 
-        // the "loader"
-        loader: "babel-loader", // or "babel" because webpack adds the '-loader' automatically
+        // "test" is commonly used to match the file extension
+        test: /(\.js|\.jsx)$/,
+
         query: {
-          presets: ['es2015']
+          presets: ['es2015','react']
         }
       }
     ]
